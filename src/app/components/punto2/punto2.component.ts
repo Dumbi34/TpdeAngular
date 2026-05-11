c5ea48af75b1beef41979e2590b44722cb1127a5
Juan Sebastian BW <juanseloeb@gmail.com>
2026-05-11 00:12:19 -0300

    Finalizados Punto 1 y Punto 2: Slide de eventos y Carrito con estilos neon

--- src/app/components/punto2/punto2.component.ts
+++ src/app/components/punto2/punto2.component.ts
@@ -1,27 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-punto2',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule],
   templateUrl: './punto2.component.html',
   styleUrl: './punto2.component.css'
 })
 export class Punto2Component {
-  monto: number = 0;
-  monedaDestino: string = 'USD';
-  resultado: number = 0;
+  productos = [
+    { nombre: 'notebook asus 13l', descripcion: 'disco 40gb, 15 pulgadas', img: 'https://picsum.photos/400/300?random=1', precio: 45.5 },
+    { nombre: 'monitor lg 14', descripcion: 'pantalla led alta definicion', img: 'https://picsum.photos/400/300?random=2', precio: 99 },
+    { nombre: 'teclado mecanico', descripcion: 'luces rgb switches blue', img: 'https://picsum.photos/400/300?random=3', precio: 25.0 }
+  ];
 
-  // Tasas de conversión fijas para el ejercicio
-  tasas: any = {
-    USD: 0.0011, 
-    EUR: 0.0010,
-    BRL: 0.0055
-  };
+  carrito: any[] = [];
+  total: number = 0;
 
-  convertir() {
-    this.resultado = this.monto * this.tasas[this.monedaDestino];
+  agregarAlCarrito(prod: any) {
+    const existe = this.carrito.find(item => item.nombre === prod.nombre);
+    if (!existe) {
+      this.carrito.push(prod);
+      this.calcularTotal();
+    } else {
+      alert('Este producto ya está en tu carrito.');
+    }
+  }
+
+  calcularTotal() {
+    this.total = this.carrito.reduce((sum, item) => sum + item.precio, 0);
+  }
+
+  limpiarCarrito() {
+    this.carrito = [];
+    this.total = 0;
   }
 }
